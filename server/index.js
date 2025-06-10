8a4edf4dfcca976e9dc51b894060017e57abc559
عبدالوهاب بوكمار <88195937+w1ty@users.noreply.github.com>
2025-06-10 11:44:02 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,12 +5,7 @@ import path from "path";
 import mysql from "mysql2";
 import db from './db.js';
 
-const employees = JSON.parse(
-fs.readFileSync(path.resolve("src/data/employees.json"), "utf-8")
-);
-const logins = JSON.parse(
-fs.readFileSync(path.resolve("src/data/logins.json"), "utf-8")
-);
+
 
 const app = express();
 const PORT = 3000;
